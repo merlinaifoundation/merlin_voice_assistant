bbad87522f4b6a5f820b30e71792cdeaa2d9f4cc
Fulvio Farina Arboccò <77405664+play-station@users.noreply.github.com>
2024-06-15 07:23:43 -0400

    v0.3.0
    Create main copy.py
    
    voiceMaker

--- libs/greeter.py
+++ libs/greeter.py
@@ -3,9 +3,8 @@ import time
 from decouple import config
 
 from libs.actions import Action
-from libs.textToSpeech import TextToSpeech
 from libs.textResponder import TextDisplay
-
+from libs.voiceMaker import VoiceMaker
 
 class Greeter(Thread):
 
@@ -13,29 +12,17 @@ class Greeter(Thread):
         super().__init__()
         timeNow = time.time()
 
-        self.sleepingVoiceTxt = config("SLEEPING_VOICE")
-        self.awakeVoiceTxt = config("AWAKE_VOICE")
         self.pv_access_key = config("PV_ACCESS_KEY")
         self.wakeWordFile = config("WAKE_WORD_FILE")
-        self.waitVoiceTxt = config("WAIT_VOICE")
-        self.initVoiceTxt = config("INIT_VOICE")
-        self.processVoiceTxt = config("PROCESSING_VOICE")
-
         self.stopWordFile = config("STOP_WORD_FILE")
 
-        self._sleepingInnerFile = "StopVoice.mp3"
-        self._awakeInnerFile = "WakeVoice.mp3"
-        self._waitInnerFile = "WaitVoice.mp3"
-        self._initInnerFile = "InitVoice.mp3"
-        self._processInnerFile = "ProcessVoice.mp3"
-
-        self._prepareInitialVoice()
-        self._prepareDefaultVoices()
         self.stopMode = 1
         self.wakeAction = None
         self.stopAction = None
         self._greeted = False
         self.count = 0
+        
+        self.voiceMaker = VoiceMaker()
         
         diff = round(time.time() - timeNow, 2)
         self._prGreen("Greeter Creation in seconds: ", diff)
@@ -46,61 +33,6 @@ class Greeter(Thread):
     def _prGreen(self,skk, number):
         print("\033[92m {}\033[00m".format(skk),number)
         
-    def _prepareInitialVoice(self):
-
-        self._initVoiceObj = TextToSpeech()
-        if self._initVoiceObj.SetFile(self._initInnerFile) is False:
-            self._initVoiceObj.PrepareFileFromText(self.initVoiceTxt)
-        print("Initializing...")
-        self.VoiceInit()
-
-    def _prepareDefaultVoices(self):
-
-        self._defaultVoiceObj = TextToSpeech()
-
-        self._waitVoiceObj = TextToSpeech()
-        if self._waitVoiceObj.SetFile(self._waitInnerFile) is False:
-            self._waitVoiceObj.PrepareFileFromText(self.waitVoiceTxt)
-            print("Creating Wait Audio File...")
-        self.VoiceWait()
-
-        self._processVoiceObj = TextToSpeech()
-        if self._processVoiceObj.SetFile(self._processInnerFile) is False:
-            self._processVoiceObj.PrepareFileFromText(self.processVoiceTxt)
-            print("Creating Process Audio File...")
-
-        self._sleepVoiceObj = TextToSpeech()
-        if self._sleepVoiceObj.SetFile(self._sleepingInnerFile) is False:
-            self._sleepVoiceObj.PrepareFileFromText(self.sleepingVoiceTxt)
-            print("Creating Stop Audio File...")
-
-        self._awakeVoiceObj = TextToSpeech()
-        if self._awakeVoiceObj.SetFile(self._awakeInnerFile) is False:
-            self._awakeVoiceObj.PrepareFileFromText(self.awakeVoiceTxt)
-            print("Creating Wake Audio File...")
-
-        print("Finished creating the TTS Defaults")
-
-    def VoiceSleeping(self):
-        self._sleepVoiceObj.SpeakFromFile(self._sleepingInnerFile)
-
-    def VoiceAwake(self):
-        self._awakeVoiceObj.SpeakFromFile(self._awakeInnerFile)
-
-    def VoiceWait(self):
-        self._waitVoiceObj.SpeakFromFile(self._waitInnerFile)
-
-    def VoiceProcess(self):
-        self._processVoiceObj.SpeakFromFile(self._processInnerFile)
-
-    def VoiceInit(self):
-        self._initVoiceObj.SpeakFromFile(self._initInnerFile)
-
-    def VoiceDefault(self, content, stopObj):
-
-        self._defaultVoiceObj.SetForceStopObj(stopObj)
-        self._defaultVoiceObj.SpeakFromText(content)
-
     def HasGreeted(self):
         return self._greeted
 
@@ -109,9 +41,6 @@ class Greeter(Thread):
 
     def ResetStopper(self):
         self.stopAction = None
-
-    def IsIdle(self):
-        return (self._defaultVoiceObj is not None) and self._defaultVoiceObj.Finished()
 
     def InitWaker(self):
         timeNow = time.time()
