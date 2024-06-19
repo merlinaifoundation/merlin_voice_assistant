4fcefa943a8999d23d3b3c5d890e97eac9cc44db
Fulvio Farina Arboccò <77405664+play-station@users.noreply.github.com>
2024-06-19 18:05:16 -0400

    Update merlin.sh

--- merlin.sh
+++ merlin.sh
@@ -1,6 +1,9 @@
 #!/bin/bash
 pulseaudio --start
-sleep 5
+sleep 0.1
+cd /home/merlin/Documents/Merlin/merlin_voice_assistant
+git stash
+git pull
 python3 -m venv myenv
 source /home/merlin/Documents/Merlin/merlin_voice_assistant/myenv/bin/activate
 pip install -r requirements.txt
